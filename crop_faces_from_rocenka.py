3d1a2b05cbab89854f8026dacda69aff08b7955b
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-06-29 09:17:20 +0200

    branch for peak_oli to deploy to js

--- crop_faces_from_rocenka.py
+++ crop_faces_from_rocenka.py
@@ -7,7 +7,7 @@ def crop_faces(trida):
     print(trida)
     # os.makedirs(trida, exist_ok=True)
     # img = face_recognition.load_image_file("skeny/{}.png".format(trida))
-    img = face_recognition.load_image_file("skeny\IA8KmentovaMarie.jpg")
+    img = face_recognition.load_image_file("zskeny/{}.png".format(trida))
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img_rgb_copy = img_rgb.copy()
 
@@ -19,34 +19,36 @@ def crop_faces(trida):
     face_locations = face_recognition.face_locations(img_rgb)
     # print("after")
     lpnb = 0
-    for top, right, bottom, left in face_locations:
+    for top, right, bottom, left in tqdm(face_locations):
         lpnb = lpnb + 1
         # Draw a box around the face
         #cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        crop_img = img_rgb[top:bottom, left:right]
+        offset = 120
+
+        crop_img = img_rgb[top-offset:bottom+offset, left-offset:right+offset]
 
 
 
         ## Calculate center points and rectangle side length
-        width = right - left
-        height = bottom - top
-        cX = left + width // 2
-        cY = top + height // 2
-        M = (abs(width) + abs(height)) / 2
+        # width = right - left
+        # height = bottom - top
+        # cX = left + width // 2
+        # cY = top + height // 2
+        # M = (abs(width) + abs(height)) / 2
 
 
         ## Get the resized rectangle points
-        newLeft = max(0, int(cX - scale_factor * M))
-        newTop = max(0, int(cY - scale_factor * M))
-        newRight = min(img_rgb.shape[1], int(cX + scale_factor * M))
-        newBottom = min(img_rgb.shape[0], int(cY + scale_factor * M))
+        # newLeft = max(0, int(cX - scale_factor * M))
+        # newTop = max(0, int(cY - scale_factor * M))
+        # newRight = min(img_rgb.shape[1], int(cX + scale_factor * M))
+        # newBottom = min(img_rgb.shape[0], int(cY + scale_factor * M))
 
 
         ## Draw the circle and bounding boxes
-        cv2.circle(img_rgb_copy, (cX, cY), radius=0, color=(0, 0, 255), thickness=2)
-        cv2.rectangle(img_rgb_copy, (left, top), (right, bottom), (0, 0, 255), 2)
-        cv2.rectangle(img_rgb_copy, (newLeft, newTop), (newRight, newBottom), (255, 0, 0), 2)
+        # cv2.circle(img_rgb_copy, (cX, cY), radius=0, color=(0, 0, 255), thickness=2)
+        # cv2.rectangle(img_rgb_copy, (left, top), (right, bottom), (0, 0, 255), 2)
+        # cv2.rectangle(img_rgb_copy, (newLeft, newTop), (newRight, newBottom), (255, 0, 0), 2)
 
 
         ## Show the original image in window resized to double
@@ -55,17 +57,16 @@ def crop_faces(trida):
         # cv2.imshow("image", crop_img)
         # cv2.waitKey(100)
         # cv2.imwrite('{}/{}.png'.format(trida, lpnb), crop_img)
-        cv2.imwrite('skeny/{}.png'.format(lpnb), crop_img)
+        cv2.imwrite('test/{}.png'.format(lpnb), crop_img)
 
 
 
     # cv2.destroyAllWindows()
 
-inputs_jpg = os.listdir("skeny/")
-inputs = []
-for a in tqdm(inputs_jpg):
-    l = len(a)
-    short = a[:l - 4]
-    crop_faces(short)
-# crop_faces("IA6")
-# crop_faces("IB6")
+# inputs_jpg = os.listdir("skeny/")
+# inputs = []
+# for a in tqdm(inputs_jpg):
+#     l = len(a)
+#     short = a[:l - 4]
+#     crop_faces(short)
+crop_faces("IB6")
